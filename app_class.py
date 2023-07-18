d9ad374449ccf1fc4feadbd4bf1ed0832cf27fd2
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-18 10:30:22 +0800

    add some user info

--- app_class.py
+++ app_class.py
@@ -13,17 +13,23 @@ class User:
     def __init__(self):
         self.__name = None
         self.__contact_no = None
+        self.__address = None
+        self.__temp = None
+        self.__destination = None
         
     def get_user_info(self):
         self.__name = input("Enter your name: ")
         self.__contact_no = int(input("Enter your contact number: "))
+        self.__address = input("Enter your current address: ")
+        self.__temp = float(input("Enter your temperature: "))
+        self.__destination = input("Enter your Destination: ")
 
     def save_info(self, writer):
         with open("user_info.csv", "w", newline="") as csvfile:
             writer = csv.writer(csvfile)
 
-            writer.writerow(["Name", "Contact Number"])
-            writer.writerow([self.__name, self.__contact_no])
+            writer.writerow(["Name", "Contact Number", "Address", "Temperature", "Destination"])
+            writer.writerow([self.__name, self.__contact_no, self.__address, self.__temp, self.__destination])
 
 if __name__ == "__main__":
     user = User()
