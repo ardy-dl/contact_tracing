00b83cab8ff91e464cadc1b88580eb6913b03910
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-18 11:20:46 +0800

    error now fixed in symptoms

--- hdf_class.py
+++ hdf_class.py
@@ -2,22 +2,27 @@
 
 class Content:
     def __init__(self):
-        self.__symptoms = []
+        self.__symptoms = {}
 
     def get_symptoms(self):
         print("Do you have any of the following: (yes/no)")
-        symptoms_list = ["Sore Throat", "Fever", "Cough", "Runny Nose", "Loss of Sense of Smell", "Loss of Sense of Taste", "Abdominal Pain", "Diarrhea"]
-        for i in symptoms_list:
+        symptoms_list = ["Sore Throat ", "Fever ", "Cough ", "Runny Nose ", "Loss of Sense of Smell ", "Loss of Sense of Taste ", "Abdominal Pain ", "Diarrhea "]
+        for symptom in symptoms_list:
             while True:
-                answer = input(i).lower
+                answer = input(symptom).lower()
                 if answer == "yes" or answer == "no":
-                    self.__symptoms[i] = answer
+                    self.__symptoms[symptom] = answer
+                    break
 
     def display(self):
         print("Symptoms:")
-        for i, answer in self.__symptoms.items():
-            print("{i} : {answer}")
+        for symptom, answer in self.__symptoms.items():
+            print(symptom,":", answer)
 
-hdf = Content
-hdf.get_symptoms()
-hdf.display()
+def main():
+    hdf = Content()
+    hdf.get_symptoms()
+    hdf.display()
+
+if __name__ == "__main__":
+    main()
