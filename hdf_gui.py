f9b2a0e9081e6bd3db0f7df45a65f3517f698f82
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-22 10:57:22 +0800

    added gui for entry labels

--- hdf_gui.py
+++ hdf_gui.py
@@ -4,24 +4,33 @@ from app_class import User
 
 class HealthDeclarationFormApp:
     def __init__(self, root):
-        self.root = root
-        self.root.title("Health Declaration Form")
-        self.set_window_position()
+        self.__root = root
+        self.__root.title("Health Declaration Form")
+        self.__set_window_position()
         self.content = User()
+        self.create_widgets()
+        
+    def __set_window_position(self): 
+        screen_width = self.__root.winfo_screenwidth()
+        screen_height = self.__root.winfo_screenheight()
 
-    def set_window_position(self):
-        # Get the screen width and height
-        screen_width = self.root.winfo_screenwidth()
-        screen_height = self.root.winfo_screenheight()
+        x = int((screen_width - 600) / 2)
+        y = int((screen_height - 400) / 2)
 
-        # Calculate the x and y position for the window to be centered
-        x = int((screen_width - 100) / 2)
-        y = int((screen_height - 600) / 2)
+        self.__root.geometry(f"600x400+{x}+{y}")
 
-        # Set the window's geometry to center it on the screen
-        self.root.geometry(f"100x600+{x}+{y}")
+    def create_widgets(self):
+        labels = ["Name", "Contact Number", "Address", "Temperature", "Destination"]
+        for i, label in enumerate(labels):
+            tk.Label(self.__root, text=label + ":").grid(row=i, column=0, padx=10, pady=5, sticky="nsew")
+            entry = tk.Entry(self.__root, width=50)  # Increase the width here
+            entry.grid(row=i, column=1, columnspan=2, padx=10, pady=5, sticky="ew")
+
+
+    def run(self):
+        self.__root.mainloop()
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = HealthDeclarationFormApp(root)
-    root.mainloop()
+    app.run()
