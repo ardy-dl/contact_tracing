d673387c9f820d057746d2dcec845b3c43baeb2e
unknown <rosedenisejdeleon@iskolarngbayan.pup.edu.ph>
2023-07-22 11:11:14 +0800

    add the vaccination but error in selection because user can modify

--- hdf_gui.py
+++ hdf_gui.py
@@ -23,8 +23,16 @@ class HealthDeclarationFormApp:
         labels = ["Name", "Contact Number", "Address", "Temperature", "Destination"]
         for i, label in enumerate(labels):
             tk.Label(self.__root, text=label + ":").grid(row=i, column=0, padx=10, pady=5, sticky="nsew")
-            entry = tk.Entry(self.__root, width=50)  # Increase the width here
+            entry = tk.Entry(self.__root, width=50) 
             entry.grid(row=i, column=1, columnspan=2, padx=10, pady=5, sticky="ew")
+
+
+        vaccination_status_choices = ["Not Yet", "1st Dose", "2nd Dose", "1st Booster Shot", "2nd Booster Shot"]
+        tk.Label(self.__root, text="Vaccination Status:").grid(row=5, column=0, sticky="e")
+        self.__vaccination_status_var = tk.StringVar(self.__root)
+        self.__vaccination_status_var.set("Not Yet")
+        self.__vaccination_status_dropdown = ttk.Combobox(self.__root, state="readonly", textvariable=self.__vaccination_status_var, values=vaccination_status_choices)
+        self.__vaccination_status_dropdown.grid(row=5, column=1)
 
 
     def run(self):
